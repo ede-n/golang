09aef3f5484026883fd9229b517bfd3d9828e018
Naveen Babu Ede <nede@gofundme.com>
2020-04-22 13:24:21 -0700

    integrates a new package

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module ede-n/hello
+module github.com/ede-n/golang
 
 go 1.14
+
+require github.com/google/go-cmp v0.4.0
--- hello.go
+++ hello.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+  	"fmt"
+	"github.com/ede-n/golang/morestrings"
+	"github.com/google/go-cmp/cmp"
+)
 
 func main() {
-    fmt.Println("Hello, World\n")
+    fmt.Println(morestrings.ReverseRunes("Hello, World\n"))
+    fmt.Println(cmp.Diff("Hello World", "Hello Go"))
+
 }
